9b29d1648a1c47e75cdb10fc0f94490bd352cac3
talgat <15talgat@mail.ru>
2022-12-11 21:56:45 +0300

    done_zadacha_3

---
+++ zadacha_3/Program.cs
@@ -0,0 +1,20 @@
+﻿double Factorial(int n)
+{
+    // 1! = 1
+    // 0! = 0
+    if(n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * Factorial(n-1);
+    }
+}
+
+System.Console.WriteLine(Factorial(5)); // 3! = 1 * 2 * 3 = 6
+
+for (int i = 1; i < 40 ; i++)
+{
+    System.Console.WriteLine($"{i}! = {Factorial(i)}");
+}
